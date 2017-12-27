f5b096d37f5f2f3546782a881d8ea86741997c48
mavstronaut <mavbarona@gmail.com>
2017-12-27 18:26:51 -0500

    js indents

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-window.onscroll=function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementbyId("btn-top").style.display="block";
-    } else {
-        document.getElementbyId("btn-top").style.display = "none";
-    }
+window.onscroll = function() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementbyId("btn-top").style.display = "block";
+	} else {
+		document.getElementbyId("btn-top").style.display = "none";
+	}
 
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
